4465e026842abc424101497fa415a521910bab0f
Alex <98969983+DangerBirdStrikesAgain@users.noreply.github.com>
2023-01-02 16:34:45 +0000

    just committing stuff bc wifi is bad
    started antientropy and states

--- EpidemicMAC/config.py
+++ EpidemicMAC/config.py
@@ -1,8 +1,21 @@
 from micropython import const
 
+# States we can be in
+LISTEN = 0
+SEND_HELLO = 1
+QUIET = 2
+RECEIVED_HELLO = 3
+
+
 # Packet types - if we needed more room for sending perhaps make this
-# 4 bits and the address 4 bits?
+# 4 bits and the address 4 bits? (actually I think that would really limit the network size nvm)
 HELLO = const(0x00)
+RTS = const(0x01)
+CTS = const(0x02)
+# Data Send frame - what is about to be sent
+DS = const(0x03)
+DATA = const(0x04)
+ACK = const(0x05)
 
 # For broadcast
 BROADCAST = const(0xFF)
@@ -15,14 +28,18 @@ FREQUENCY = 443
 
 # Hello timer - a new hello message is broadcast when this time elapses 
 # remember that is in ms
-HELLOTIMER = 30000
+HELLO_TIMER = 30000
 
 # Quantity of time to wait for the board to transmit the packet
 TRANSMIT_TIMEOUT = 2.0
-
 # Quantitiy of time to wait for the board to receive a packet
 RECEIVE_TIMEOUT = 0.5
+# How long to try to send RTS es 
+RTS_TIMEOUT = 3.0
 
+# The number of attempts that should be made to send packets before giving up
+RTS_REENTRIES = 3
+DATA_REENTRIES = 3
 
 # self.ack_retries = 5
 #         """The number of ACK retries before reporting a failure."""
--- EpidemicMAC/epidemic.py
+++ EpidemicMAC/epidemic.py
@@ -1,3 +1,26 @@
+"""
+A Circuit Python implmentation of the epidemic routing protocol with media access control
+TODO - put the paper it is based on here
+ 
+Uses Raspberry Pi Pico with the Adafruit Ultimate GPS Breakout V3 and RFM69HCW Radio breakout
+Requires rfm69 library and config files
+ 
+Wiring:
+Radio   GPS   Pico
+------------------
+GND            GND
+VIN            3V3
+SCK            GP2
+MISO           GP0
+MOSI           GP3
+CS             GP1
+RST            GP4
+"""
+
+# TODO - Error handling
+
+
+
 import config
 import rfm69
 import board
@@ -5,18 +28,79 @@ import time
 import busio
 import digitalio
 
+# Set up the radio
 spi = busio.SPI(board.GP2, MOSI=board.GP3, MISO=board.GP0)
 cs = digitalio.DigitalInOut(board.GP1)
 reset = digitalio.DigitalInOut(board.GP4)
 rfm69 = rfm69.RFM69(spi, cs, reset, config.FREQUENCY)
+# TODO - Initialise GPS? 
+
+# List of nodes we have contacted recently
+contacted = []
+
+# Set the state to the starting state 
+state = 0
+
+def log(message):
+    # TODO - perhaps change to adding to a logging file 
+    print(message)
 
 def getGPS():
+    # TODO - should this be a string? Is there a better way of doing this?
     return ("10.284638, 89.473057")
 
 def sendHello():
     gps = getGPS()
     return rfm69.send(data = bytes(gps, "utf-8"), destination = config.BROADCAST, packetType = config.HELLO)
-    
+
+def sendRTS(sender):
+    # TODO - Something about a dict of the messages that we have and how to deal with them - key could be first 4 bytes as the node then the time - we can get this from the GPS? 
+    data = "hello!! This is an RTS"
+    return rfm69.send(data = bytes(data, "utf-8"), destination = sender, packetType = config.RTS)
+
+def antiEntropy(sender):
+    RTSCount = 0 
+    #something about the RTS timeout and checking it for interrupt
+    while RTSCount<config.RTS_REENTRIES:
+        sendRTS(sender)
+        RTSCount+=1
+
+def sendToAppLayer(item):
+    # TODO - implement this using global variable
+    return True
+
+def handleReceive(args):
+    if args == None:
+        return config.LISTEN
+    elif args[1] == config.HELLO:
+        return config.RECEIVED_HELLO
+    elif args[1] == config.CTS and args[2]!=config.ADDRESS:
+        # TODO - implment a timer so we don't happen to just zone out
+        return config.QUIET
+
 while True:
-    print(sendHello())
-    time.sleep(1)
+    if state == config.LISTEN:
+        args = rfm69.receive()
+        state = handleReceive(args)
+
+    elif state == config.SEND_HELLO:
+        if sendHello():
+            state = config.LISTEN
+        else:
+            log("Failed to send hello")
+
+    elif state == config.RECEIVED_HELLO:
+        sender = args[2]
+        packet = args[4]
+        sendToAppLayer(packet)
+        if sender not in contacted:
+            if sender>config.ADDRESS:
+                antiEntropy(sender) # This returns true or false let's use it for logging
+                # TODO - move on the state
+                """
+                You're implementing the antiEntropy stuff here :) and making sure that all the packets are 
+                """
+
+    elif state==config.QUIET:
+        # TODO - timer
+        pass
